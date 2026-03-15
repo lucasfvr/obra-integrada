5de390a42d705f4164e8c10ed5c8b258a811e75d
Victor220205 <medeirosvictor401@gmail.com>
2026-03-15 12:11:25 -0300

    Add files via upload

---
+++ frontend/vite-project/src/view/components/FeaturedInfo.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './FeaturedInfo.css';
+
+export default function FeaturedInfo() {
+  return (
+    <div className="featured">
+      <div className="featured-item">
+        <span className="featuredTitle">Receita</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">R$8,415</span>
+          <span className="featuredMoneyRate">
+            -11.4
+            <i class="bi bi-arrow-down bad-rate"></i>
+          </span>
+        </div>
+
+        <span className="featuredResult">Comparado ao último mês</span>
+      </div>
+
+      <div className="featured-item">
+        <span className="featuredTitle">Vendas</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">R$4,415</span>
+          <span className="featuredMoneyRate">
+            -1.4
+            <i class="bi bi-arrow-down bad-rate"></i>
+          </span>
+        </div>
+
+        <span className="featuredResult">Comparado ao último mês</span>
+      </div>
+
+      <div className="featured-item">
+        <span className="featuredTitle">Custo da Operação</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">R$2,225</span>
+          <span className="featuredMoneyRate">
+            +2.4
+            <i class="bi bi-arrow-up good-rate"></i>
+          </span>
+        </div>
+
+        <span className="featuredResult">Comparado ao último mês</span>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/vite-project/src/view/components/WidgetSm.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import './WidgetSm.css';
+
+export default function WidgetSm() {
+  return (
+    <div className="widgetSm">
+      <h2 className="widgetSmTitle">Novos Usuários</h2>
+      <ul className="usersList">
+        <li className="listItem">
+          <img
+            src="https://images.unsplash.com/photo-1648562951003-5acb85b16aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fFM0TUtMQXNCQjc0fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+            alt="user-avatar"
+            className="widgetUserImg"
+          />
+
+          <div className="widgetSmInfo">
+            <span className="widgetUserUsername">Lucas Montenegro</span>
+            <span className="widgetUserTitle">Engenheira de Software</span>
+          </div>
+
+          <button className="widgetSmButton">
+            <i class="bi bi-eye-fill"></i>
+            Display
+          </button>
+        </li>
+        <li className="listItem">
+          <img
+            src="https://images.unsplash.com/photo-1648562951003-5acb85b16aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fFM0TUtMQXNCQjc0fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+            alt="user-avatar"
+            className="widgetUserImg"
+          />
+
+          <div className="widgetSmInfo">
+            <span className="widgetUserUsername">Lucas Montenegro</span>
+            <span className="widgetUserTitle">Engenheira de Software</span>
+          </div>
+
+          <button className="widgetSmButton">
+            <i class="bi bi-eye-fill"></i>
+            Display
+          </button>
+        </li>
+        <li className="listItem">
+          <img
+            src="https://images.unsplash.com/photo-1648562951003-5acb85b16aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fFM0TUtMQXNCQjc0fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+            alt="user-avatar"
+            className="widgetUserImg"
+          />
+
+          <div className="widgetSmInfo">
+            <span className="widgetUserUsername">Lucas Montenegro</span>
+            <span className="widgetUserTitle">Engenheira de Software</span>
+          </div>
+
+          <button className="widgetSmButton">
+            <i class="bi bi-eye-fill"></i>
+            Display
+          </button>
+        </li>
+        <li className="listItem">
+          <img
+            src="https://images.unsplash.com/photo-1648562951003-5acb85b16aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fFM0TUtMQXNCQjc0fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+            alt="user-avatar"
+            className="widgetUserImg"
+          />
+
+          <div className="widgetSmInfo">
+            <span className="widgetUserUsername">Lucas Montenegro</span>
+            <span className="widgetUserTitle">Engenheira de Software</span>
+          </div>
+
+          <button className="widgetSmButton">
+            <i class="bi bi-eye-fill"></i>
+            Display
+          </button>
+        </li>
+        <li className="listItem">
+          <img
+            src="https://images.unsplash.com/photo-1648562951003-5acb85b16aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fFM0TUtMQXNCQjc0fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+            alt="user-avatar"
+            className="widgetUserImg"
+          />
+
+          <div className="widgetSmInfo">
+            <span className="widgetUserUsername">Lucas Montenegro</span>
+            <span className="widgetUserTitle">Engenheira de Software</span>
+          </div>
+
+          <button className="widgetSmButton">
+            <i class="bi bi-eye-fill"></i>
+            Visualizar
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
